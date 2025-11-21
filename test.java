d8e2f1b2be45fa6314c9d217db0e47e7ae7a3e01
Asif <ruashraicseasif@gmail.com>
2025-11-21 19:39:17 +0600

    Add additional print statement for location

--- test.java
+++ test.java
@@ -3,6 +3,7 @@ public class test {
         System.out.println("hello");
         System.out.println("My name is asif");
                 System.out.println("I live in Rajshahi");
+                System.out.println("I live in baya");
 
 
     }
