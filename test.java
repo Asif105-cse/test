6a4d3fe227058f3822e6e1436d07093e471f065d
habibur rahman <jgcasif1037@gmail.com>
2025-11-21 19:34:43 +0600

    second commit

--- test.java
+++ test.java
@@ -2,6 +2,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("My name is asif");
+                System.out.println("I live in Rajshahi");
+
 
     }
     
